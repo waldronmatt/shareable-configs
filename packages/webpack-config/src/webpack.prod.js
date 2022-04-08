64717f6ffbb2e20c8a374a213081fe56ed276ae0
waldronmatt <mhuntwaldron@gmail.com>
2022-04-08 17:55:09 -0400

    feat(webpack.prod.js): remove minification from webpack.prod

--- packages/webpack-config/src/webpack.prod.js
+++ packages/webpack-config/src/webpack.prod.js
@@ -1,17 +1,9 @@
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
-
 const productionConfig = {
   mode: 'production',
   // fairly quick build speed that's recommended for production use
   devtool: 'source-map',
   optimization: {
     minimize: true,
-    minimizer: [
-      new ESBuildMinifyPlugin({
-        target: 'es2015',
-        css: true,
-      }),
-    ],
   },
   performance: {
     hints: 'warning',
