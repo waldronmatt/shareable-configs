516ee4e5a20065677e5d171b61495de532d10076
waldronmatt <mhuntwaldron@gmail.com>
2022-05-26 21:53:44 -0400

    fix(postinstall.js): move config to separate object

--- packages/jest-config/scripts/postinstall.js
+++ packages/jest-config/scripts/postinstall.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 const filePath = path.join(process.env.INIT_CWD, 'jest.config.js');
 
+const fileConfigObject = {
+  preset: '@waldronmatt/jest-config',
+};
+
 if (!fs.existsSync(filePath)) {
   fs.writeFileSync(
     filePath,
-    `module.exports = { preset: '@waldronmatt/jest-config' };`
+    `module.exports = ${JSON.stringify(fileConfigObject, undefined, 2)}`
   );
 }
