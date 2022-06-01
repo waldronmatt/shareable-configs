dea6b843506c556129c80658a5046a75e1bad38e
waldronmatt <mhuntwaldron@gmail.com>
2022-05-31 22:00:57 -0400

    feat(postcss-config): add postcss-normalize lib

--- packages/postcss-config/README.md
+++ packages/postcss-config/README.md
@@ -22,7 +22,9 @@ module.exports = require('@waldronmatt/postcss-config');
 
 - `autoprefixer`
 - `postcss-100vh-fix`
+- `postcss-cli`
 - `postcss-flexbugs-fixes`
+- `postcss-normalize`
 - `postcss-preset-env`
 
 ## License
--- packages/postcss-config/index.js
+++ packages/postcss-config/index.js
@@ -1,14 +1,21 @@
 // https://nextjs.org/docs/advanced-features/customizing-postcss-config
+const postcssNormalize = require('postcss-normalize');
 
 module.exports = {
-  plugins: {
-    'postcss-flexbugs-fixes': {},
-    'postcss-100vh-fix': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
+  plugins: [
+    'postcss-flexbugs-fixes',
+    'postcss-100vh-fix',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
       },
-      stage: 3,
-    },
-  },
+    ],
+    postcssNormalize({
+      forceImport: 'sanitize.css',
+    }),
+  ],
 };
