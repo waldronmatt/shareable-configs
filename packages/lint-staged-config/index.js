7f20ffe4397c4b51bf2b9f975a7bcce920f78a42
waldronmatt <mhuntwaldron@gmail.com>
2022-06-20 22:24:07 -0400

    fix(index.js): update glob match for all non-js/css files, have prettier ignore unknown files

--- packages/lint-staged-config/index.js
+++ packages/lint-staged-config/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 module.exports = {
+  '!(*.{js,jsx,ts,tsx,css,scss})': ['prettier --write --ignore-unknown'],
   '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --cache --fix'],
   '*.{css,scss}': ['prettier --write', 'stylelint --cache --fix'],
-  '*.{md,html,json}': ['prettier --write'],
 };
