cc915813cce202b407b9cfb3a80e7eb40c7785e5
waldronmatt <mhuntwaldron@gmail.com>
2022-11-04 23:59:58 -0400

    fix(lint-staged.config.js): remove yml linting during staging

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 const config = require('./packages/lint-staged-config');
-const ymlConfig = require('./packages/lint-staged-config/yml');
 
 module.exports = {
   '*': ['secretlint'],
-  ...ymlConfig,
   ...config,
 };
