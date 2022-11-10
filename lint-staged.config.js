3e1a390fffb05427d118b38776fb42f685abacae
waldronmatt <mhuntwaldron@gmail.com>
2022-11-09 20:43:49 -0500

    fix(lint-staged.config.js): add back yml to lint-staged and fix yml linting issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 const config = require('./packages/lint-staged-config');
+const ymlConfig = require('./packages/lint-staged-config/yml');
 
 module.exports = {
   '*': ['secretlint'],
+  ...ymlConfig,
   ...config,
 };
