a9071ad0c5e25755062b8e8c727d3dc0fa2f83bf
waldronmatt <mhuntwaldron@gmail.com>
2022-11-25 14:02:11 -0500

    feat(index.js): remove json, md, and css/scss linting
    BREAKING CHANGE: remove non-js references for user to opt-in

--- packages/lint-staged-config/index.js
+++ packages/lint-staged-config/index.js
@@ -1,7 +1,4 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 module.exports = {
-  '*.md': ['prettier --cache --write', 'markdownlint'],
   '*.{js,jsx,ts,tsx}': ['prettier --cache --write', 'eslint --cache --fix'],
-  '*.json': ['prettier --cache --write'],
-  '*.{css,scss}': ['prettier --cache --write', 'stylelint --cache --fix'],
 };
