05b12c5e8799c6ae85f64c2aa824a1c5ad8b3fd3
waldronmatt <mhuntwaldron@gmail.com>
2022-11-25 13:28:01 -0500

    feat(jest-preset.js): remove ignore and alias mappers for per-project opt-in
    BREAKING CHANGE: removed transformIgnorePatterns and coveragePathIgnorePatterns

--- packages/jest-config/jest-preset.js
+++ packages/jest-config/jest-preset.js
@@ -1,23 +1,8 @@
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-  moduleNameMapper: {
-    /*
-      method for stubbing Webpack resource queries
-      https://github.com/visualfanatic/vue-svg-loader/issues/73#issuecomment-478295746
-
-      should match any file extension containing any '?' resource
-      queries with '@' webpack alias
-    */
-    '^@/(.*.)(\\?(.*))$': '<rootDir>/src/$1',
-    // resolve '@' Webpack alias
-    // https://stackoverflow.com/a/65578141
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '.+\\.(css|scss|apng|avif|gif|jpe?g|png|svg|webp|woff2?)$':
       'jest-transform-stub',
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
 };
